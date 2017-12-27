4335dbf1afcb34b0a023e94aae581051b39d47af
Duoc UC <Duoc UC@B33-02>
2017-12-27 20:19:12 -0300

    David: Conexion fue agregado con su respectivo codigo a la base de datos Se diseño la vista modificar. Se diseño la vista de menu inicial.

--- src/vista/Menuinicio.java
+++ src/vista/Menuinicio.java
@@ -27,21 +27,76 @@ public class Menuinicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        MenuSalir = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        MenuAgregar = new javax.swing.JMenuItem();
+        MenuModificar = new javax.swing.JMenuItem();
+        MenuEliminar = new javax.swing.JMenuItem();
+        MenuConsulta = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 279, Short.MAX_VALUE)
+        );
+
+        jMenu1.setText("Archivo");
+
+        MenuSalir.setText("Salir");
+        jMenu1.add(MenuSalir);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Procesos");
+
+        MenuAgregar.setText("Agregar");
+        MenuAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MenuAgregarActionPerformed(evt);
+            }
+        });
+        jMenu2.add(MenuAgregar);
+
+        MenuModificar.setText("Modificar");
+        jMenu2.add(MenuModificar);
+
+        MenuEliminar.setText("Eliminar");
+        jMenu2.add(MenuEliminar);
+
+        MenuConsulta.setText("Consulta");
+        jMenu2.add(MenuConsulta);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void MenuAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MenuAgregarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MenuAgregarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +134,14 @@ public class Menuinicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem MenuAgregar;
+    private javax.swing.JMenuItem MenuConsulta;
+    public javax.swing.JMenuItem MenuEliminar;
+    private javax.swing.JMenuItem MenuModificar;
+    private javax.swing.JMenuItem MenuSalir;
+    public javax.swing.JMenu jMenu1;
+    public javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
