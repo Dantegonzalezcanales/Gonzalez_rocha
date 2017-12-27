516532d0cf91394dc93cdbbafc430426457fe05b
Duoc UC <Duoc UC@B33-02>
2017-12-27 19:21:22 -0300

    David: Agregado packages modelo y vista, con algunos de sus contenido, para empezar a trabajar en ellos.

--- src/modelo/Modelo.java
+++ src/modelo/Modelo.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controlador;
+package modelo;
 
 /**
  *
  * @author Duoc UC
  */
-public class Rochadavid_gonzalezdante {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Modelo {
     
 }
